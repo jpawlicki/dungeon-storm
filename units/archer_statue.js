02773ce8b47cbb8810cdfa664100d980f38d9968
Joshua Pawlicki <gilgarn@gmail.com>
2021-01-03 20:04:52 -0800

    Archer statue and some rooms with it.

---
+++ units/archer_statue.js
@@ -0,0 +1,13 @@
+{
+	let unit = {
+		ai: [aiData.ATTACK_NEAREST],
+		abilities: [abilityData.SHOOT],
+		id: "Archer Statue",
+		portrait: "archer_statue.png",
+		strengths: [5, 5, 3, 5],
+		strengthsFrightened: [4, 4, 2, 4],
+		threats: [false, false, false, false],
+		threatsFrightened: [false, false, false, false],
+	};
+	unitData[unit.id] = unit;
+}
